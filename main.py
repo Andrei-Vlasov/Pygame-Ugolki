8d4fe406528734dcb530f8d4a490c00d41e47388
Andrey Vlasov <59191751+Andrei-Vlasov@users.noreply.github.com>
2020-06-19 10:02:13 +0300

    v1.0
    it works

--- main.py
+++ main.py
@@ -70,10 +70,10 @@ class Tile(pygame.sprite.Sprite):
 
 def swap(pos1, pos2):
     pos1.piece, pos2.piece = pos2.piece, pos1.piece
-    print(pos1.column, pos1.row, pos1.piece, 'to', pos2.column, pos2.row, pos2.piece)
     pos1.draw()
     pos2.draw()
     sound.play()
+
 
 
 tile_list = pygame.sprite.Group()
@@ -92,18 +92,102 @@ def redraw():
     pygame.display.update()
     tile_list.update()
 
+
 def legal(pos1, pos2):
-    return True
+    if pos1.piece != team:
+        return False
+    if base is not None and pos1 != base:
+        return False
+    if pos2 in visited:
+        return False
+    if pos2.piece != 0:
+        return False
+    if pos1.column != pos2.column and pos1.row != pos2.row:
+        return False
+
+    if abs(pos1.row - pos2.row) > 2 or abs(pos1.column - pos2.column) > 2:
+        return False
+
+    if abs(pos1.row - pos2.row) == 2 and pos1.column == pos2.column:
+        for middle in tile_list:
+            if middle.row == min(pos1.row, pos2.row)+1 and middle.column == pos1.column:
+                if middle.piece != 0:
+                    return True
+        return False
+
+    if abs(pos1.column - pos2.column) == 2 and pos1.row == pos2.row:
+        for middle in tile_list:
+            if middle.column == min(pos1.column, pos2.column) + 1 and middle.row == pos1.row:
+                if middle.piece != 0:
+                    return True
+        return False
+
+    if base is None:
+        return True
+    else:
+        return False
+
+
+def win():
+    black_stack = 0
+    white_stack = 0
+
+    for elem in tile_list:
+        for row1 in range(6, 9):
+            for column1 in range(1, 5):
+                if elem.row == row1 and elem.column == column1:
+                    if elem.piece == 1:
+                        white_stack += 1
+                    elif turn >= 80 and elem.piece == -1:
+                        print('White won')
+                        return True
+
+        for row2 in range(1, 4):
+            for column2 in range(5, 9):
+                if elem.row == row2 and elem.column == column2:
+                    if elem.piece == -1:
+                        black_stack += 1
+                    elif turn >= 79 and elem.piece == 1:
+                        print('Black won')
+                        return True
+
+    if white_stack == 12:
+        print('White won')
+        return True
+
+    elif black_stack == 12:
+        print('Black won')
+        return True
+
+    return False
 
 
 running = True
 team = 1
 move = []
+base = None
+visited = []
+turn = 1
+
+
 while running:
-    pygame.time.delay(60)
+    pygame.time.delay(FPS)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE and base is not None:
+            base = None
+            visited = []
+            move = []
+            turn += 1
+            team *= -1
+
+            print('Крок', turn, 'гравця ', end='')
+            if team == 1:
+                print('білими')
+            else:
+                print('чорними')
+
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
             pos = pygame.mouse.get_pos()
             for i in tile_list:
@@ -112,10 +196,15 @@ while running:
                     if len(move) == 2:
                         if legal(move[0], move[1]):
                             swap(move[0], move[1])
-                            move = []
-                            team *= -1
+                            running = not win()
+                            base = move[1]
+                            visited.append(move[0])
+                            move = [base]
                         else:
-                            move.remove(move[0])
+                            if base is None:
+                                move.remove(move[0])
+                            else:
+                                move.remove(move[1])
 
     redraw()
 
